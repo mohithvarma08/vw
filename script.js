5b3a6af0cd918527df297f261db1fdfc5a875367
mohithvarma08 <mohithvarmaps@gmail.com>
2026-02-14 22:14:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 function openLetter(){
-    document.querySelector(".envelope").classList.add("open");
-    document.getElementById("openBtn").style.display="none";
-
-    setTimeout(()=>{
-        document.getElementById("letterCard").style.display="block";
-    },600);
+    document.querySelector(".envelope").style.display="none";
+    document.getElementById("letterCard").style.display="block";
 }
 
 function goToQuestion(){
@@ -18,36 +14,39 @@ function celebrate(){
     startConfetti();
 }
 
-/* ===== CONFETTI ===== */
+/* CONFETTI */
 
 const canvas = document.getElementById("confettiCanvas");
 const ctx = canvas.getContext("2d");
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 let confetti = [];
 
 function startConfetti(){
-    for(let i=0;i<150;i++){
+    for(let i=0;i<120;i++){
         confetti.push({
             x:Math.random()*canvas.width,
             y:Math.random()*canvas.height,
             r:Math.random()*6+4,
-            d:Math.random()*150
+            d:Math.random()*3+2
         });
     }
-    drawConfetti();
+    animate();
 }
 
-function drawConfetti(){
+function animate(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
     ctx.fillStyle="#ff2f7d";
-    confetti.forEach((c,i)=>{
+
+    confetti.forEach(c=>{
         ctx.beginPath();
         ctx.arc(c.x,c.y,c.r,0,Math.PI*2);
         ctx.fill();
-        c.y+=2;
+        c.y+=c.d;
         if(c.y>canvas.height) c.y=0;
     });
-    requestAnimationFrame(drawConfetti);
+
+    requestAnimationFrame(animate);
 }
