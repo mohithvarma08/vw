535469ada928a9105a53b4cdf3809406c2907410
mohithvarma08 <mohithvarmaps@gmail.com>
2026-02-14 21:24:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const images = document.querySelectorAll(".dream");
 
 images.forEach((img, index) => {
 img.style.opacity = "0";
-img.style.transform += " scale(0.9)";
+img.style.transform += " scale(0.95)";
 
 setTimeout(() => {
 img.style.transition = "0.8s ease";
 img.style.opacity = "1";
-img.style.transform = img.style.transform.replace(" scale(0.9)", "");
+img.style.transform = img.style.transform.replace(" scale(0.95)", "");
 }, index * 120);
 });
 
